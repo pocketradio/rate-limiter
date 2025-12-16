41e1a5a6aa7ff0c1fd37bb9547743a403c5054e2
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-12-16 13:18:13 +0530

    removed globals to use app.state and added noscript recovery

--- main.py
+++ main.py
@@ -1,58 +1,81 @@
 from redis import asyncio as redis
 import os
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from dotenv import load_dotenv
 load_dotenv()
 from contextlib import asynccontextmanager
 import time
 import aiofiles
+from redis.exceptions import NoScriptError, RedisError
+
 
 REDIS_HOST: str = os.getenv("REDIS_HOST")
 RATE_LIMIT = 10
 WINDOW_SECONDS = 60
-PORT: str = os.getenv("REDIS_PORT")
+REDIS_PORT: str = os.getenv("REDIS_PORT")
 
-redis_client = None
-lua_script_contents = None
-lua_script_hash = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     
     pool = redis.ConnectionPool(
         host = REDIS_HOST,
-        port = int(PORT),
+        port = int(REDIS_PORT),
         db = 0
     )
-    global redis_client
-    global lua_script_contents
-    global lua_script_hash
     
     redis_client = redis.Redis(connection_pool=pool)
     
     async with aiofiles.open('./lua_scripts/fixed_window_rate_limit.lua', mode='r') as f:
-        lua_script_contents = await f.read()
+        lua_script_contents_fixed = await f.read()
+            
+    lua_script_hash_fixed = await redis_client.script_load(lua_script_contents_fixed) # redis permanently stores the script on the server and returns a SHA1 hash. 
+    # so now we only have to send the hash over the network instead of the entire lua script. ( server side cachingg scripts with EVALSHA )        
         
-    lua_script_hash = await redis_client.script_load(lua_script_contents) # redis permanently stores the script on the server and returns a SHA1 hash. 
-    # so now we only have to send the hash over the network instead of the entire lua script. ( server side cachingg scripts with EVALSHA )
+    async with aiofiles.open('./lua_scripts/sliding_window_rate_limit.lua', mode= 'r') as f:
+        lua_script_contents_sliding = await f.read()
+    
+    lua_script_hash_sliding = await redis_client.script_load(lua_script_contents_sliding)
+    
+    
+    app.state.redis = redis_client
+    app.state.sliding_window_sha = lua_script_hash_sliding
+    app.state.fixed_window_sha = lua_script_hash_fixed
+    app.state.script_contents_fixed = lua_script_contents_fixed
+    
+    
     yield
+    
+    await redis_client.close()
+    
     
 app = FastAPI(lifespan=lifespan)
 
-def key_generation(id):
+def key_generation(user_id):
 
     current_time = time.time()
     current_time_window = current_time // WINDOW_SECONDS
     
-    return f"rate_limit:{id}:{current_time_window}"
+    return f"rate_limit:{user_id}:{current_time_window}"
 
 
-@app.get("/limit/{user_id}")
-async def send_hash(user_id : int):
+@app.get("/fixed_limit/{user_id}")
+async def rate_limit_check_fixed_window(user_id : int, request: Request):
     
-    user_key = key_generation(id= user_id)
-    result = await redis_client.evalsha(lua_script_hash, 1, user_key, RATE_LIMIT, WINDOW_SECONDS  ) # evalsha docs show arguments to be strictly positional, hence.
+    redis_client = request.app.state.redis
+    sha_fixed = request.app.state.fixed_window_sha
+    
+    user_key = key_generation(user_id= user_id)
+    try:
+        result = await redis_client.evalsha(sha_fixed, 1, user_key, RATE_LIMIT, WINDOW_SECONDS  ) # evalsha docs show arguments to be strictly positional, hence.
 
+    except NoScriptError:
+        sha = await redis_client.script_load(request.app.state.script_contents_fixed)
+        request.app.state.fixed_window_sha = sha
+        result = await redis_client.evalsha(sha, 1, user_key, RATE_LIMIT, WINDOW_SECONDS)
+    except RedisError:
+        raise HTTPException(500, "rate limiter unavailable.")
+    
     if result == 0:
         raise HTTPException(status_code=429, detail='Rate limit reached. goodbye child')
 
